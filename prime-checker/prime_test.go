e9e44ed30d76c1da40aabfedc682e88120877d4a
Burak Can Karasel <burakcankarasel@gmail.com>
2022-05-14 11:07:30 +0300

    test is completed to with a %88.9 pass

---
+++ prime-checker/prime_test.go
@@ -0,0 +1,38 @@
+package main
+
+import "testing"
+
+var tests = []struct {
+	Input  int
+	Output bool
+	Err    bool
+}{
+	{-3, false, true},
+	{2, true, false},
+	{4, false, false},
+	{5, true, false},
+	{9, false, false},
+	{47, true, false},
+	{49, false, false},
+}
+
+// test folder is for some of my cases
+
+// and here with TestPrime func i test my cases by iterating tests
+
+func TestPrime(t *testing.T) {
+	for _, tt := range tests {
+		result, err := primeCheck(tt.Input)
+		if tt.Err {
+			if err == nil {
+				t.Error("Expected an error but didnt get one")
+			}
+		} else {
+			if err != nil {
+				t.Error("Did not expect error but got :", err)
+			} else if result != tt.Output {
+				t.Errorf("Expected %v but got %v", tt.Output, result)
+			}
+		}
+	}
+}
